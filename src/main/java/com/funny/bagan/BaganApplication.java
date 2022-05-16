67bf745c795323a00381ded5597f802ed5290b94
kgggh <68227318+kgggh@users.noreply.github.com>
2022-05-16 23:54:08 +0900

    init project bagan~~~~~~!!!!!!!!!

---
+++ src/main/java/com/funny/bagan/BaganApplication.java
@@ -0,0 +1,13 @@
+package com.funny.bagan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funny/bagan/BaganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funny.bagan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
